0448cf064c013181fff4d5ae4e7a75091f01bf73
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-19 00:24:16 +0500

    MULTIQUERY

--- forfinals.php
+++ forfinals.php
@@ -10,10 +10,20 @@ if($conn->connect_error){
     die("error occured ".$conn->connect_error);
 }
 
-$sql="insert into webEngg2 (fname,lname) values('imran','khan')
+$sql="insert into webEngg2 (fname,lname) values('imran','khan');
     ";
-    if($conn->query($sql)===TRUE){
-        echo 'Table Created';
+
+$sql.="insert into webEngg2 (fname,lname) values('ali','mughal');
+    ";
+
+$sql.="insert into webEngg2 (fname,lname) values('basit','ahmed');
+    ";
+
+$sql.="insert into webEngg2 (fname,lname) values('junaid','khan');
+    ";
+
+    if($conn->multi_query($sql)===TRUE){
+        echo 'INSERTED  SUCCESSFULLY';
     }
     else{
         echo 'Error occured'.$conn->error();
